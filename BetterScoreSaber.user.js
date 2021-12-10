f77c582bbe1a509a3fae33f32e863ed06692a0e0
MicroCBer <66859419+MicroCBer@users.noreply.github.com>
2021-12-11 00:32:42 +0800

    Update BetterScoreSaber.user.js

--- BetterScoreSaber.user.js
+++ BetterScoreSaber.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BetterScoreSaber
 // @namespace    https://github.com/MicroCBer
-// @version      0.4
+// @version      0.5
 // @description  Add some features to ScoreSaber
 // @author       MicroBlock
 // @match        https://scoresaber.com/**
